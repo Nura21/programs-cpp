35f1b893f1e065453ad1e80dbd78cb297df7f2e6
Nura21 <70548701+Nura21@users.noreply.github.com>
2021-03-25 12:14:45 +0700

    Add files via upload

---
+++ PERULANGAN/findSimilarDesimal.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main (){
+float angka;
+int a=0;
+cout<<"Masukkan Bilangan : ";
+cin>>angka;
+    a=angka;
+    if (angka==a){
+        cout<<"Bilangan Bulat";
+    }else{
+        cout<<"Bilangan Pecahan";
+    }
+}
+
+//referensi : http://castle-techno.blogspot.com/2017/11/program-menentukan-bilangan-bulat-atau-pecahan.html
